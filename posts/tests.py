f3798125bf39d188ace833443d93b2ea9eb6f70d
piet <pprotassow@posteo.de>
2023-10-18 11:10:19 +0200

    test added

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is a test!")
+    
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_url_available_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_template_name_correct(self):
+        resp = self.client.get(reverse('home'))
+        self.assertTemplateUsed(resp, 'home.html')
+
+    def test_template_name_content(self):
+        resp = self.client.get(reverse('home'))
+        self.assertContains(resp, '<h1>Message board homepage</h1>')
+        self.assertContains(resp, '<li>This is a test!</li>')
+
--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,9 @@ from django.views.generic import ListView
 from .models import Post
 
 class HomepageView(ListView):
-    
     model = Post
     template_name = "home.html"
-    post_list = Post.objects.all()
+    # context_object_name = "items"
+    # post_list = Post.objects.all()
 
 
